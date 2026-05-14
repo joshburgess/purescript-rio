1c287cb81f11076b96b3317c8a1eb1047e37c874
Josh Burgess <joshburgesswebdev@gmail.com>
2026-05-14 16:31:06 -0400

    test(stm/thub): pin unbounded delivery order to every subscriber
    The unit pin fixes `[1, 2, 3]`. Generalize so the property
    survives regressions that only manifest on empty, singleton, or
    larger publish batches. Every subscriber observes every value
    in publish order, independently of any other subscriber.

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -42,6 +42,7 @@ import Test.RIO.STMSpec as STMSpec
 import Test.RIO.STM.TMapSpec as TMapSpec
 import Test.RIO.STM.TMap.PropertiesSpec as TMapPropertiesSpec
 import Test.RIO.STM.THubSpec as THubSpec
+import Test.RIO.STM.THub.PropertiesSpec as THubPropertiesSpec
 import Test.RIO.STM.TQueueSpec as TQueueSpec
 import Test.RIO.STM.TQueue.PropertiesSpec as TQueuePropertiesSpec
 import Test.RIO.STM.TSemaphoreSpec as TSemaphoreSpec
@@ -85,6 +86,7 @@ main = runSpecAndExitProcess [ consoleReporter ] do
   TMapPropertiesSpec.spec
   TSemaphoreSpec.spec
   THubSpec.spec
+  THubPropertiesSpec.spec
   TracerSpec.spec
   MetricsSpec.spec
   LocalSpec.spec
